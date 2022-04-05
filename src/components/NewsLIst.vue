cd9e0873eb557989cfec9289de92ed712923523f
Jaun-Luc <jaunlucbrown15@gmail.com>
2022-04-05 01:00:04 -0500

    fix titles

--- src/components/NewsLIst.vue
+++ src/components/NewsLIst.vue
@@ -1,15 +1,36 @@
 <template>
     <ul class="news__list">
-        <li class="news__item">News item 1</li>
+
+        <li class="news__item" v-for="article in articles">{{ article.title }}</li>
+        <!-- <li class="news__item">News item 1</li>
         <li class="news__item">News item 2</li>
-        <li class="news__item">News item 3</li>
+        <li class="news__item">News item 3</li> -->
     </ul>
 </template>
 
 <script>
 export default {
     data() {
-        return {};
+        return {
+            articles: []
+        };
     },
+    created() {
+        let self = this;
+
+        fetch('https://newsapi.org/v2/top-headlines?country=us',
+{
+    headers: {
+        'Authorization':  `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+    }
+})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            self.articles = data.articles;
+        });
+    }
 };
 </script>
