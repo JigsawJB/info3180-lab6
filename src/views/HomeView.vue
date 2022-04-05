26c3acb6e0c18b57e0a8d051fa370096aa57ba80
Jaun-Luc <jaunlucbrown15@gmail.com>
2022-04-04 23:07:56 -0500

    added news items

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,14 @@
 <script>
+
+import NewList from '@/components/NewsList.vue'
+
 export default {
     data() {
         return {
-            message: "Hello World! Welcome to VueJS"
+            message: "Hello Sir! Welcome to my VueJS. You can call me Jigsaw."
         };
     },
+    components: { NewList }
 }
 </script>
 
@@ -13,6 +17,7 @@ export default {
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
+        <NewList />
       </div>
     </div>
 </template>
